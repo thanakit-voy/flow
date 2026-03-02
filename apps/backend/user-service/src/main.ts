d51519c7fb9fc0d734e4c858e78286b9ef4598be
Thanakit Konthong <thanakit.voy@gmail.com>
2026-03-02 18:53:40 +0700

    fix: initial project

--- apps/backend/user-service/src/main.ts
+++ apps/backend/user-service/src/main.ts
@@ -9,9 +9,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'users';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
--- apps/frontend/shell/src/app/app.routes.ts
+++ apps/frontend/shell/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: 'dashboard',
-    loadChildren: () => import('dashboard/Routes').then((m) => m!.remoteRoutes),
+    loadChildren: () => import('dashboard/Routes').then((m) => m.remoteRoutes),
   },
   {
     path: '',
