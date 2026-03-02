f1df682e7bdbaf3125377c3f974e039468fd6eaa
Thanakit Konthong <thanakit.voy@gmail.com>
2026-03-02 20:34:58 +0700

    feat(dashboard): enable standalone component for remote entry

--- apps/frontend/dashboard/src/app/remote-entry/entry.ts
+++ apps/frontend/dashboard/src/app/remote-entry/entry.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NxWelcome } from './nx-welcome';
 
 @Component({
+  standalone: true,
   imports: [NxWelcome],
   selector: 'app-dashboard-entry',
   template: `<app-nx-welcome></app-nx-welcome>`,
